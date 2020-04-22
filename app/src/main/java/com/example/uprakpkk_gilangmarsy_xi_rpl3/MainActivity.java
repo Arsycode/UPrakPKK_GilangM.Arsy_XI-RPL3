a2f4a3424e92cbbcb06242968b8c26ea58491b01
Arsycode <gilangmarsy2.siswa@smkn4bdg.sch.id>
2020-04-22 15:19:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uprakpkk_gilangmarsy_xi_rpl3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.uprakpkk_gilangmarsy_xi_rpl3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
